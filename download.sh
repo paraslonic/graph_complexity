b04b274398e5e9b679472d276d1fb6e0fc4f3ee2
Александр Манолов <manolov@pyxis.ripcm.com>
2018-07-19 13:07:54 +0300

    assembly summary as parameter

--- download.sh
+++ download.sh
@@ -1,19 +1,22 @@
 calc(){ awk "BEGIN { print int($*) }"; }
-COMPLETE_GENOMES=$(grep "$1" ../assembly_summary.txt | grep "Complete" | wc -l)
-ALL_GENOMES=$(grep "$1" ../assembly_summary.txt | wc -l)
+ASM_FILE="$2"
+COMPLETE_GENOMES=$(grep "$1" $ASM_FILE | grep "Complete" | wc -l)
+ALL_GENOMES=$(grep "$1" $ASM_FILE | wc -l)
 echo -e "GENOMES. All: $ALL_GENOMES\tComplete:$COMPLETE_GENOMES"
 if [ $COMPLETE_GENOMES -ge 50 ]
 then
 	echo "will download only complete genomes"
-	wget `grep "$1" ../assembly_summary.txt | grep "Complete" | awk -F "\t" '{print $20}' | awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}'`
+	wget `grep "$1" $ASM_FILE | grep "Complete" | awk -F "\t" '{print $20}' | awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}'`
 else
 	if [ $ALL_GENOMES -le 100  ]
 	then
-		wget `grep "$1" ../assembly_summary.txt | awk -F "\t" '{print $20}' | awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}'`
+		echo "will download all genomes"
+		wget `grep "$1" $ASM_FILE | awk -F "\t" '{print $20}' | awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}'`
 	else
-		grep "$1" ../assembly_summary.txt | grep -v "Complete" | awk -F "\t" '{print $20}' | awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}' > ncomplete_genomes.url
+		echo "will download random subset of genomes"
+		grep "$1" $ASM_FILE | grep -v "Complete" | awk -F "\t" '{print $20}' | awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}' > ncomplete_genomes.url
 		shuf -n $(calc 100 - $COMPLETE_GENOMES) ncomplete_genomes.url > selected_genomes.url
-		grep "$1" ../assembly_summary.txt | grep "Complete" | awk -F "\t" '{print $20}' | awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}' >> selected_genomes.url
+		grep "$1" $ASM_FILE | grep "Complete" | awk -F "\t" '{print $20}' | awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}' >> selected_genomes.url
 		wget $(cat selected_genomes.url )
 	fi
 fi
